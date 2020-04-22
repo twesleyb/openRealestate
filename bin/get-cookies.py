a03c36574d7cb00813fd91542fdfed49dc24dc2c
twesleyb <tyler.w.bradshaw@duke.edu>
2020-04-22 07:52:23 -0400

    im a cookie monster

--- bin/get-cookies.py
+++ bin/get-cookies.py
@@ -1,42 +1,56 @@
 #!/usr/bin/env python3
 
-# Using selenium.
-
-import os
-import re
-import sys
-import json
-from datetime import date
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-## Default parameters.
-LOG_LEVEL = 3
-META_EXTENSION = ".json"
-DATA_EXTENSION = ".tsv.gz"
-BASEURL = "https://cells.ucsc.edu/"
-CHROME_DRIVER = "/home/twesleyb/src/chromedriver.exe"
+import requests
+from torrequest import TorRequest
 
-# Chrome options and path to chromedriver.
-chrome = CHROME_DRIVER 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('log-level=' + str(LOG_LEVEL))
-chrome_options.add_experimental_option('excludeSwitches',['enable-logging'])
+# Must install with pip. 
+# Install pip with conda into env.
+# Then insure you have unset pip
+# $ unset pip
+# $ unset pip3
+#https://conda.io/projects/conda/en/latest/user-guide/getting-started.html#installing-a-package-with-pip
+# /home/twesleyb/anaconda3/envs/pipenv/bin/pip install <package>
 
-# Start headless chromedriver session.
-driver = webdriver.Chrome(chrome,options=chrome_options)
-driver.get(BASEURL)
+## Defaults:
+url="https://www.zillow.com/"
+TORPASS='16:84205A404D64FF5F60EB72627A012BB308919ACA14AB093C6F9379EAD7'
 
-# All dataset buttons.
-# While loop to insure that we successfully collect buttons.
-buttons = []
-while len(buttons) <= 0:
-    buttons = driver.find_elements_by_class_name('list-group-item')
-# Iterate through buttons, get page source data.
-soup = list()
-for button in buttons:
-    button.click()
-    soup.append(BeautifulSoup(driver.page_source,"xml"))
+def get_cookies(url):
+    ''' Get a websites cookies given its url. '''
+    ## Default parameters.
+    LOG_LEVEL = 3 # What does log level do?
+    CHROME_DRIVER = '/home/twesleyb/bin/chromium/chromedriver.exe'
+    # Chrome options and path to chromedriver.
+    chrome = CHROME_DRIVER 
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('log-level=' + str(LOG_LEVEL))
+    chrome_options.add_experimental_option('excludeSwitches',['enable-logging'])
+    # Start headless chromedriver session.
+    driver = webdriver.Chrome(chrome,options=chrome_options)
+    driver.get(url)
+    # Get cookies.
+    cookies = driver.get_cookies()
+    return(cookies)
+# EOF
 
+cookies = get_cookies(url)
+
+# Randomize IP address with tor.
+# pip install torrequest
+def random_ip(password):
+    ''' Reset tor to randomize your password given your tor hashed control
+    password. Requires that you set HashedControlPassword in .torrc'''
+    # Add hashed passwordexit()
+    r=TorRequest(password)
+    # Reset Tor.
+    tr.reset_identity()
+    # Check new ip.
+    response= tr.get('http://ipecho.net/plain')
+    print ("New IP Address",response.text)
+# EOF
+
+random_ip(password=TORPASS)
