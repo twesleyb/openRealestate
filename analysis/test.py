8c21d06c91b0a953238f30b40f58467ac4b30082
twesleyb <tyler.w.bradshaw@duke.edu>
2020-04-26 14:33:06 -0400

    why tor not working?

--- analysis/test.py
+++ analysis/test.py
@@ -1,53 +1,79 @@
 #!/usr/bin/env python3
 
-# Using selenium + tor.
-# Install with tbselenium with pip. Insure that you use the pip in your virtual
-# environment.
-# $ $(which pip) install tbselenium
-
-# Paths to stuff.
-# I tried putting geck on the Linux side of things and adding it to my path, but
-# this didn't work...
-#tor = '/mnt/c/oprojects/zillow_scraper/extensions/tor-browser_en-US'
-
-tor = '/mnt/c/Users/User/Downloads/Tor Browser/Browser'
-firefox = '/mnt/c/Program Files/Mozilla Firefox/firefox.exe'
-
-
-# Selenium + tor
-#tor="/usr/bin/tor"
-#tor="/usr/sbin/tor"
-#tor="/etc/tor"
-tor="/usr/share/tor"
-#tor="/usr/share/man/man1/tor.1.gz"
-from tbselenium.tbdriver import TorBrowserDriver
-with TorBrowserDriver(tor) as driver:
-    driver.get('https://check.torproject.org')
-
-## Driving tor with selenium.
+#--------------------------------------------------------------------
+## Test 1.
+#--------------------------------------------------------------------
+# Driving tor with selenium + geckodriver.
 
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-# Paths to tor-firefox and geckodriver.
+# Path to Tor-Firefox and geckodriver executables (Windows).
 firefox = '/mnt/c/Users/User/Downloads/Tor Browser/Browser/firefox.exe'
 gecko = '/mnt/c/Program Files/Mozilla Firefox/geckodriver.exe'
 
-driver = webdriver.Firefox(firefox_binary=binary,executable_path=gecko)
-
-driver = webdriver.Firefox(executable_path=gecko)
-
-# Create webdriver.
+# Create the webdriver.
 binary = FirefoxBinary(firefox)
 driver = webdriver.Firefox(firefox_binary=binary,executable_path=gecko)
 
-# Results in an ERROR :
+# ERROR :
 # SessionNotCreatedException: Message: Unable to find a matching set of capabilities
 
-# But this works...
+# Show that geckodriver works...
 driver = webdriver.Firefox(executable_path=gecko)
 url="https://www.google.com/"
 driver.get(url)
 
-# But, it isn't through tor.
+# But, it isn't tor.
 driver.get('https://check.torproject.org')
+
+#--------------------------------------------------------------------
+## Test 2.
+#--------------------------------------------------------------------
+# Show that we can randomize ip with torreqeust.
+
+import os
+import sys
+import requests
+from os.path import dirname
+from torrequest import TorRequest
+
+# Directories.
+here = os.getcwd()
+root = dirname(here)
+sys.path.append(root)
+
+# Load Functions in root/Py
+import Py
+from Py.get_pass import get_pass
+
+# Add HashedControlPass.
+password = get_pass("torpass")
+tr=TorRequest(password=password)
+
+# Reset Tor.
+tr.reset_identity()
+
+# Check initial ip.
+session = requests.session()
+response = session.get('http://ipecho.net/plain')
+ip = response.text
+print("IP address is set to: {}".format(ip))
+
+# Check new ip with tor.
+response = tr.get('http://ipecho.net/plain')
+ip = response.text
+print("IP address is set to: {}".format(ip))
+
+# Check if tor is active.
+response = tr.get('https://check.torproject.org')
+response.text # Sorry, you are not using Tor.
+
+#--------------------------------------------------------------------
+## Test 3.
+#--------------------------------------------------------------------
+# We can use torrify on the command line.
+
+torify wget -O - 'https://check.torproject.org' 
+
+# Congratulations. This browser is configured to use Tor.
