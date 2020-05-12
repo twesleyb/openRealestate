edbf8f8cc486da9f16cb7cb18e8a782ee70ef59b
twesleyb <twesleyb10@gmail.com>
2020-05-12 10:23:27 -0400

    update status reports

--- durham/main.py
+++ durham/main.py
@@ -16,6 +16,7 @@
 # * Write data to file!!!
 
 # Imports.
+import sys
 from webscraper import utils
 from webscraper import gomaps
 from webscraper import addresses
@@ -29,20 +30,26 @@ durham = addresses.durham
 while len(durham.addr_list) > 0:
     ## Get a random address.
     address = durham.random()
+    msg = ' '.join([address.get('NUMBER'), address.get('STREET'),
+        address.get('CITY'), address.get('POSTCODE')])
+    print('Searching for: {}...'.format(msg), file=sys.stderr)
     # Search for address.
     response = gomaps.find_address(driver,address)
     # Check response.
     if response is None:
+        print('... Address not found.', file = sys.stderr)
         gomaps.append_errors(address,'STDERR')
         driver.refresh()
-        sleep(5)
+        utils.zzz(5)
         continue
     ## Add nearby parcels to results.
+    print('Address found. Adding nearby parcels to results...',file=sys.stderr)
     response = gomaps.add_buffer(driver,start=1500,increase_by=500)
     ## Download results, and then load into python.
     gomaps.download_results(driver)
     results = gomaps.load_results()
     ## Update results.
-    append_results(results)
+    print('Updating results.\n',file=sys.stderr)
+    gomaps.append_results(results)
 # EOL
 
