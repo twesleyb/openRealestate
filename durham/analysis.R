436c3f48b260517e069412bbf6f00f1a10e346b6
twesleyb <tyler.w.bradshaw@duke.edu>
2020-05-22 21:43:03 -0400

    combine_complexes - can we combine complex names?

--- durham/analysis.R
+++ durham/analysis.R
@@ -1,61 +1,47 @@
 #!/usr/bin/env Rscript
 
+## Input Arguments.
+input_data = "durham-realestate.csv"
+
 # Load renv.
 renv::load(getrd()) # NOTE: getrd() is an alias in my .Rprofile.
 # Alternatively, you can use:
 #renv::load("your/path/to/open-realestate/")
 
-library(dplyr)
-library(data.table)
-library(tidygeocoder)
-
-## Input Arguments.
-input_data = "durham-realestate.csv"
+# Imports:
+suppressPackageStartupMessages({
+	library(dplyr)
+	library(data.table)
+	#library(tidygeocoder)
+})
 
 # Path to durham realestate data.
 here <- getwd()
 datadir <- file.path(here,"data",input_data)
 
 # Load the data.
-dt <- data.table::fread(datadir)
+durham <- data.table::fread(datadir)
 
-## Let's look at Milan Woods.
+# All neighborhood subdivisions.
+subdivisions <- unique(durham$SUBD_DESC)
+n_subd <- length(subdivisions)
+message(paste("Number of subdivisions:",formatC(n_subd,big.mark=",")))
 
-# Subset the data.
-subdivisions <- unique(dt$SUBD_DESC) # All subdivisions.
-milan_woods <- subdivisions[grep("MILAN",subdivisions)] # Combine Milan Woods.
-subdt <- dt %>% filter(SUBD_DESC %in% milan_woods)
-
-# Median home price:
-median_price <- formatC(median(subdt$SALE_PRICE),format='d',big.mark=',')
-median_price
-
-# Number of addresses and owners:
-n_addr <- length(unique(subdt$PARCEL_ID))
-n_owners <- length(unique(subdt$OWNER_NAME))
-
-# Median size:
-median_size <- median(subdt$SUM_ACRE)
-
-# Median duration owned.
-# Tidy-up dates.
+# Fix-up dates.
 date_format <- "%Y%d%m"
-subdt$DATE_SOLD <- as.Date(as.character(subdt$DATE_SOLD),format=date_format)
+durham$DATE_SOLD <- as.Date(as.character(durham$DATE_SOLD),format=date_format)
 
-# Median ownership.
-subdt$DUR_OWN <- as.numeric(difftime(today,subdt$DATE_SOLD,units="days"))/365
-median(subdt$DUR_OWN,na.rm=TRUE)
+# Function to calculate duration owned in years.
+dur_owned <- function(col.id,today=Sys.Date()) {
+	years_owned <- as.numeric(difftime(today,col.id,units="days"))/365
+	return(years_owned)
+}
 
+# Summarize subdivisions.
+dt <- durham %>% group_by(SUBD_DESC) %>% 
+	summarize(median_price = median(SALE_PRICE),
+		  n_parcels = length(unique(PARCEL_ID)),
+		  median_size = median(SUM_ACRE),
+		  median_dur_owned = median(dur_owned(DATE_SOLD),na.rm=TRUE))
 
-
-# Convert addresses to lat/lon.
-# To install needed: libudunits2-dev.
-# $ sudo apt-get install libudunits2-dev
-
-# Complete addresses:
-colnames(subdt)
-address <- paste(subdt$street,subdt$city,subdt$zip)
-
-subdt %>% geocode(.tbl, address, method = "census", lat = lat, long = long, ...)
-geocode(.tbl, address, method = "census", lat = lat, long = long, ...)
-
+fwrite(dt,"temp.csv")
